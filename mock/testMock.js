c8aa8b0def2c3e9d53a5c2f22fed4c372d32a151
majiansong <mjs1706@sina.com>
2021-06-28 00:20:11 +0800

    初始化项目

--- mock/testMock.js
+++ mock/testMock.js
@@ -2,6 +2,7 @@ module.exports={
     "GET /api/mockData":(req,res)=>{
         res.send({
             msg:"登录成功！",
+            success:true,
             data:[
                 {title:"name"}
             ]
